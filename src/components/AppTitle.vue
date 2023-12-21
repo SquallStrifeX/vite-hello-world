1c84ce346c41e5dd4027e338c2fd9f3dbe125e31
SquallStrifeX <squallstrifex@gmail.com>
2023-12-21 17:56:25 +0100

    end part 1

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "La mia prima app con Vite!"
+    }
+  },
+}
+</script>
+
+<template lang="">
+  <div>
+    {{message}}
+  </div>
+</template>
+
+<style lang="">
+  
+</style>
