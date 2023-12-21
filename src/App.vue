ca113439f6c70d7a39770fd0566d390ddef7d9e0
SquallStrifeX <squallstrifex@gmail.com>
2023-12-21 18:03:16 +0100

    end bonus and end exercise

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import AppTitle from './components/AppTitle.vue'
+import Jumbo from './components/jumbotron.vue'
+
 </script>
 
 <template>
   <AppTitle></AppTitle>
+  <jumbo></jumbo>
 </template>
 
 <style scoped>
---
+++ src/components/jumbotron.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div>
+        <img src="../assets/vue.svg" alt="">
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
